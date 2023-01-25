b4c0a249914b34ba42c41295abe4b8be80dab503
teguhpambudif <teguhpambudi1251@gmail.com>
2023-01-25 23:43:11 +0700

    ping app but isn't working anymore, ms forbid

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Net.NetworkInformation;
+using System.Text;
+
+Ping pingSender = new Ping();
+PingOptions options = new PingOptions();
+
+string data = "Learn to code";
+byte[] buffer = Encoding.ASCII.GetBytes(data);
+int timeout = 120;
+string address = "8.8.8.8";
+PingReply reply = pingSender.Send(address,timeout,buffer,options);
+if (reply.Status == IPStatus.Success)
+{
+    Console.WriteLine("Status {0}", reply.Status.ToString());
+}
+
+;
