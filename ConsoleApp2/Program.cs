267c21ef22dfb12286fe85bb4801897b1b841e40
teguhpambudif <teguhpambudi1251@gmail.com>
2023-01-26 19:38:22 +0700

    add reply status, but still not working lol

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -5,6 +5,8 @@ using System.Text;
 Ping pingSender = new Ping();
 PingOptions options = new PingOptions();
 
+options.DontFragment = true;
+
 string data = "Learn to code";
 byte[] buffer = Encoding.ASCII.GetBytes(data);
 int timeout = 120;
@@ -13,6 +15,9 @@ PingReply reply = pingSender.Send(address,timeout,buffer,options);
 if (reply.Status == IPStatus.Success)
 {
     Console.WriteLine("Status {0}", reply.Status.ToString());
+    Console.WriteLine("Roundtrip {0}",reply.RoundtripTime);
+    Console.WriteLine("Time to live {0}",reply.Options.Ttl);
+    Console.WriteLine("Buffer size {0}",reply.Buffer.Length);
 }
 
 ;
